aece9b4ad3d308737584d6d354437b4c9246a33f
Ebenezer <eebenezer949@gmail.com>
2024-07-05 23:51:07 +0100

    UI: figuring the widget

--- main.go
+++ main.go
@@ -1,53 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	var duration time.Duration
-	start := time.Now()
+	myApp := app.New()
 
-	// Channel to communicate commands
-	cmd := make(chan string)
+	w := myApp.NewWindow("Support Analytics")
 
-	// Goroutine to read user input
-	go func() {
-		var command string
-		for {
-			fmt.Print("cmd: ")
-			fmt.Scanln(&command)
-			cmd <- command
-			if command == "stop" {
-				break
-			}
-		}
-	}()
+	// Creating widgets for the timer, button, and data entry
+	// ======widgets-======
+	clk := widget.NewLabel("00:00:00")
+	startBtn := widget.NewButton("Start", nil)
+	stopBtn := widget.NewButton("stop", nil)
+	duration := widget.NewLabel("Call duration: 00:00:00")
+	duration.SetText(clk.Text)
+	engineerDet := widget.NewEntry()
+	engineerDet.PlaceHolder = "Support Engineer: "
+	detail1 := widget.NewEntry()
+	detail1.PlaceHolder = "Enter detail"
 
-	// Goroutine to update and print the timer
-	go func() {
-		for {
-			time.Sleep(1 * time.Second)
-			duration = time.Since(start)
-			hours := int(duration.Hours())
-			minutes := int(duration.Minutes()) % 60
-			seconds := int(duration.Seconds()) % 60
-
-			// Move cursor up one line, clear the line, and print the timer
-			fmt.Printf("\033[1A\033[2K%02d : %02d : %02d\ncmd: ", hours, minutes, seconds)
-		}
-	}()
-
-	// Wait for the "stop" command
-	for {
-		command := <-cmd
-		if command == "stop" {
-			break
-		}
-	}
-
-	duration = time.Since(start) // Ensure duration is up to date
-	fmt.Println("\nFinished")
-	fmt.Printf("The entire call duration is: %v\n", duration)
+	timerContent := container.New(layout.NewVBoxLayout(), container.New(layout.NewCenterLayout(), clk),
+	container.New(layout.NewAdaptiveGridLayout(2), startBtn, stopBtn))
+	w.SetContent(timerContent)
+	w.Show()
+	myApp.Run()
+	// timerContent
+	// entryContent
 }
